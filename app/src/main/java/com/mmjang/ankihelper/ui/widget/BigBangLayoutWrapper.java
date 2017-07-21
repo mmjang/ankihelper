0c36020a44880066fe615f09c64321347fa00c40
lowwor <lowwor@foxmail.com>
2017-07-22 00:33:44 +0800

    feat: add original BigBangLayout
    From https://github.com/l465659833/Bigbang

---
+++ app/src/main/java/com/mmjang/ankihelper/ui/widget/BigBangLayoutWrapper.java
@@ -0,0 +1,308 @@
+package com.mmjang.ankihelper.ui.widget;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.ScrollView;
+
+import com.mmjang.ankihelper.R;
+import com.mmjang.ankihelper.util.ViewUtil;
+
+public class BigBangLayoutWrapper extends FrameLayout {
+
+    private BigBangLayout mBigBangLayout;
+    private BigBangBottom mBottom;
+    private BigBangHeader mHeader;
+    private ScrollView mScrollView;
+
+    private boolean fullScreenMode=false;
+    private boolean stickHeader = false;
+
+
+    public BigBangLayoutWrapper(Context context) {
+        super(context);
+        init();
+    }
+
+    public BigBangLayoutWrapper(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public BigBangLayoutWrapper(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public BigBangLayoutWrapper(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    public void setBackgroundColorWithAlpha(int color,int alpha) {
+        alpha = (int) ((alpha / 100.0f) * 255);
+        setBackgroundColor(Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color)));
+    }
+
+    public void addTextItem(String text) {
+        mBigBangLayout.addTextItem(text);
+    }
+
+    public void reset() {
+        mBigBangLayout.reset();
+    }
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.bigbang_layout,this);
+
+        mBigBangLayout= (BigBangLayout) findViewById(R.id.bigbang);
+        mBottom= (BigBangBottom) findViewById(R.id.bottom);
+        mHeader= (BigBangHeader) findViewById(R.id.header);
+        mScrollView = (ScrollView) findViewById(R.id.bigbang_scroll);
+
+        mBigBangLayout.setActionListener(new BigBangLayout.ActionListener() {
+            @Override
+            public void onSelected(String text) {
+                if (mActionListener!=null){
+                    mActionListener.onSelected(text);
+                }
+            }
+
+            @Override
+            public void onSearch(String text) {
+                if (mActionListener!=null){
+                    mActionListener.onSearch(text);
+                }
+            }
+
+            @Override
+            public void onShare(String text) {
+                if (mActionListener!=null){
+                    mActionListener.onShare(text);
+                }
+            }
+
+            @Override
+            public void onCopy(String text) {
+                if (mActionListener!=null){
+                    mActionListener.onCopy(text);
+                }
+            }
+
+            @Override
+            public void onTrans(String text) {
+                if (mActionListener!=null){
+                    mActionListener.onTrans(text);
+                }
+            }
+
+            @Override
+            public void onDrag() {
+                if (mActionListener!=null){
+                    mActionListener.onDrag();
+                }
+            }
+
+            @Override
+            public void onDragSelectEnd() {
+                mBottom.onDragSelectEnd();
+            }
+
+            @Override
+            public void onCancel() {
+                if (mActionListener!=null){
+                    mActionListener.onCancel();
+                }
+            }
+        });
+
+        mHeader.setActionListener(new BigBangHeader.ActionListener() {
+            @Override
+            public void onSearch() {
+                mBigBangLayout.onSearch();
+            }
+
+            @Override
+            public void onShare() {
+                mBigBangLayout.onShare();
+            }
+
+            @Override
+            public void onCopy() {
+                mBigBangLayout.onCopy();
+            }
+
+            @Override
+            public void onTrans() {
+                mBigBangLayout.onTrans();
+            }
+
+            @Override
+            public void onCancel() {
+                mBigBangLayout.onCancel();
+            }
+        });
+        mBottom.setActionListener(new BigBangBottom.ActionListener() {
+            @Override
+            public void onDrag() {
+                mBigBangLayout.onDrag();
+            }
+
+            @Override
+            public void onDragSelect(boolean isDragSelcetion) {
+                mBigBangLayout.onDragSelect(isDragSelcetion);
+                if (mActionListener!=null){
+                    mActionListener.onDragSelection();
+                }
+
+            }
+
+            @Override
+            public void onSwitchType(boolean isLocal) {
+                if (mActionListener!=null){
+                    mActionListener.onSwitchType(isLocal);
+                }
+            }
+
+            @Override
+            public void onSelectOther() {
+                mBigBangLayout.onSelectOther();
+            }
+
+            @Override
+            public void onSwitchSymbol(boolean isShow) {
+                mBigBangLayout.setShowSymbol(isShow);
+                if (mActionListener!=null){
+                    mActionListener.onSwitchSymbol(isShow);
+                }
+            }
+
+            @Override
+            public void onSwitchSection(boolean isShow) {
+                mBigBangLayout.setShowSection(isShow);
+                if (mActionListener!=null){
+                    mActionListener.onSwitchSection(isShow);
+                }
+            }
+        });
+
+    }
+
+    public void onSwitchType(boolean isLocal) {
+        mBottom.setIsLocal(isLocal);
+    }
+
+
+    public void setShowSymbol(boolean showSymbol) {
+        mBottom.setShowSymbol(showSymbol);
+    }
+
+    public void setShowSection(boolean showSection) {
+        mBottom.setShowSection(showSection);
+    }
+
+    public void setFullScreenMode(boolean fullScreenMode){
+        this.fullScreenMode=fullScreenMode;
+    }
+
+    public void setStickHeader(boolean stickHeader) {
+        this.stickHeader = stickHeader;
+        mBigBangLayout.setStickHeader(stickHeader);
+        mHeader.setStickHeader(stickHeader);
+        if (stickHeader){
+            mHeader.setVisibility(VISIBLE);
+        }else {
+            mHeader.setVisibility(GONE);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mHeader.measure(widthMeasureSpec,MeasureSpec.makeMeasureSpec(0,MeasureSpec.EXACTLY));
+        int childHeight= (int) (mBottom.getMeasuredHeight()+mBigBangLayout.getMeasuredHeight() + (stickHeader?mHeader.getMeasuredHeight()*4.0/3:0));
+        if (fullScreenMode){
+            setMeasuredDimension(MeasureSpec.makeMeasureSpec(width,MeasureSpec.AT_MOST),MeasureSpec.makeMeasureSpec(getRootView().getHeight(),MeasureSpec.AT_MOST));
+        }else {
+            if (height > 0) {
+                setMeasuredDimension(getMeasuredWidth(), Math.min(childHeight, height));
+            } else {
+                setMeasuredDimension(getMeasuredWidth(), Math.max(childHeight, height));
+            }
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int topPadding = 0;
+
+        if (stickHeader){
+            topPadding += (int) (mHeader.getMeasuredHeight()*1.0/3);
+            if (fullScreenMode){
+                topPadding += mHeader.getMeasuredHeight()*2.0/3+ViewUtil.getNavigationBarHeight((Activity) getContext());
+            }
+            mHeader.layout(left,top+topPadding,right,top+topPadding+mHeader.getMeasuredHeight());
+            top = top + topPadding + mHeader.getMeasuredHeight();
+        }else {
+            if (fullScreenMode) {
+                topPadding += (int) (mHeader.getMeasuredHeight() * 2.0 / 3)+ViewUtil.getNavigationBarHeight((Activity) getContext());
+                top = top + topPadding ;
+            }
+        }
+        if (fullScreenMode){
+            if (mBigBangLayout.getMeasuredHeight()<bottom-top- mBottom.getMeasuredHeight()*3.0/3){
+                //显示在中部
+                int layoutBottom= (int) ((bottom- mBottom.getMeasuredHeight()*3.0/3+top)/2+mBigBangLayout.getMeasuredHeight()/2);
+                int layoutTop= (int) ((bottom- mBottom.getMeasuredHeight()*3.0/3+top)/2-mBigBangLayout.getMeasuredHeight()/2);
+                mScrollView.layout(left,layoutTop,right,layoutBottom);
+            }else {
+                mScrollView.layout(left, top, right, (int) (bottom - mBottom.getMeasuredHeight() * 3.0 / 3));
+            }
+            mBottom.layout(left, (int) (bottom - mBottom.getMeasuredHeight()*3.0/3), right, (int) (bottom- mBottom.getMeasuredHeight()*0.0/3));
+        }else {
+            mScrollView.layout(left, top, right, bottom - mBottom.getMeasuredHeight());
+            mBottom.layout(left, bottom - mBottom.getMeasuredHeight(), right, bottom);
+        }
+    }
+
+    public void setBottomVibility(int vibility){
+        mBottom.setVisibility(vibility);
+    }
+
+    private ActionListener mActionListener;
+
+    public void setActionListener(ActionListener actionListener) {
+        mActionListener = actionListener;
+    }
+
+    public interface ActionListener {
+        void onSelected(String text);
+
+        void onSearch(String text);
+
+        void onShare(String text);
+
+        void onCopy(String text);
+
+        void onTrans(String text);
+
+        void onDrag();
+
+        void onSwitchType(boolean isLocal);
+
+        void onSwitchSymbol(boolean isShow);
+
+        void onSwitchSection(boolean isShow);
+
+        void onDragSelection();
+
+        void onCancel();
+    }
+}
---
+++ app/src/main/java/com/mmjang/ankihelper/util/ConstantUtil.java
@@ -0,0 +1,161 @@
+package com.mmjang.ankihelper.util;
+
+public class ConstantUtil {
+
+    public static final String CONTENT="content://";
+    public static final String AUTHORITY="com.forfun.bigbang";
+    public static final String SEPARATOR= "/";
+    public static final String CONTENT_URI =CONTENT+AUTHORITY;
+
+    public static final String TYPE_STRING="string";
+    public static final String TYPE_INT="int";
+    public static final String TYPE_LONG="long";
+    public static final String TYPE_FLOAT="float";
+    public static final String TYPE_BOOLEAN="boolean";
+    public static final String TYPE_CONTAIN="contain";
+    public static final String DEFAULT_CURSOR_NAME= "default";
+    public static final String VALUE= "value";
+    public static final String NULL_STRING= "null";
+
+
+    public static final String BROADCAST_RELOAD_SETTING="broadcast_reload_setting";
+    public static final String BROADCAST_BIGBANG_MONITOR_SERVICE_MODIFIED ="broadcast_bigbang_monitor_service_modified";
+
+    public static final String BROADCAST_CLIPBOARD_LISTEN_SERVICE_MODIFIED="broadcast_clipboard_listen_service_modified";
+
+    public static final String BROADCAST_SET_TO_CLIPBOARD="broadcast_set_to_clipboard";
+    public static final String BROADCAST_SET_TO_CLIPBOARD_MSG="broadcast_set_to_clipboard_msg";
+
+
+
+
+    //shareCard
+    public static final String HAD_SHARED="had_shared";
+    public static final String SETTING_OPEN_TIMES="setting_open_times";
+
+    //FunctionSettingCard
+    public static final String MONITOR_CLIP_BOARD="monitor_clip_board";
+    public static final String MONITOR_CLICK="monitor_click";
+    public static final String TOTAL_SWITCH="total_switch";
+    public static final String SHOW_FLOAT_VIEW="show_float_view";
+    public static final String REMAIN_SYMBOL="remain_symbol";
+    public static final String REMAIN_SECTION="remain_section";
+    public static final String DEFAULT_LOCAL="default_local";
+
+
+    public static final String AUTO_OPEN_SETTING="auto_open_setting";
+
+    //floatview
+    public static final String FLOAT_SWITCH_STATE="float_switch_state";
+    public static final String FLOAT_VIEW_LAND_X="float_view_land_x";
+    public static final String FLOAT_VIEW_LAND_Y="float_view_land_Y";
+    public static final String FLOAT_VIEW_PORT_X="float_view_port_x";
+    public static final String FLOAT_VIEW_PORT_Y="float_view_port_y";
+
+
+
+    public static final String IS_SHOW_NOTIFY="is_show_notify";
+    public static final String NOTIFY_DISABLED_IGNORE="notify_disabled_ignore";
+
+
+    //FeedBackAndUpdateCard
+
+    //MonitorSettingCard
+    public static final String TEXT_ONLY="text_only";
+    public static final String QQ_SELECTION="qq_selection";
+    public static final String WEIXIN_SELECTION="weixin_selection";
+    public static final String OTHER_SELECTION="other_selection";
+
+    public static final String BROWSER_SELECTION="browser_selection";
+
+
+    public static final String Setting_content_Changes ="tencent_contents_change";
+    public static final String SHOW_TENCENT_SETTINGS = "tencent_settings";
+
+
+    public static final String ONLINE_CONFIG_OPEN_UPDATE="online_config_open_update";
+    public static final String DOUBLE_CLICK_INTERVAL="double_click_interval";
+    public static final int DEFAULT_DOUBLE_CLICK_INTERVAL = 1000;
+
+
+    //SettingBigBangActivity
+    public static final String TEXT_SIZE="text_size";
+    public static final String LINE_MARGIN="line_margin";
+    public static final String ITEM_MARGIN="item_margin";
+    public static final String ITEM_PADDING="item_padding";
+    public static final String BIGBANG_ALPHA="bigbang_alpha";
+    public static final String USE_LOCAL_WEBVIEW="use_local_webview";
+    public static final String USE_FLOAT_VIEW_TRIGGER="use_float_view_trigger";
+    public static final String BIGBANG_DIY_BG_COLOR ="bigbang_diy_bg_color";
+    public static final String IS_FULL_SCREEN ="is_full_screen";
+    public static final String IS_STICK_HEADER ="is_stick_header";
+    public static final String IS_STICK_SHAREBAR ="is_stick_sharebar";
+    public static final String AUTO_ADD_BLANKS="auto_add_blanks";
+    public static final String TREAT_BLANKS_AS_SYMBOL="treat_blanks_as_symbol";
+
+
+
+    public static final String FLOATVIEW_SIZE ="floatview_size_";
+    public static final String FLOATVIEW_ALPHA ="floatview_alpha";
+    public static final String FLOATVIEW_DIY_BG_COLOR ="floatview_diy_bg_color";
+    public static final String FLOATVIEW_IS_STICK ="floatview_is_stick";
+
+
+    public static final int DEFAULT_TEXT_SIZE=14;
+    public static final int DEFAULT_LINE_MARGIN=8;
+    public static final int DEFAULT_ITEM_MARGIN=0;
+    public static final int DEFAULT_ITEM_PADDING=10;
+
+
+    //whiteListActivity
+    public static final String WHITE_LIST_COUNT ="white_list_count";
+    public static final String WHITE_LIST ="white_list";
+    public static final String REFRESH_WHITE_LIST_BROADCAST ="refresh_white_list_broadcast";
+
+
+    //FloatwhiteListActivity
+    public static final String FLOAT_WHITE_LIST_COUNT ="float_white_list_count";
+    public static final String FLOAT_WHITE_LIST ="float_white_list_";
+    public static final String FLOAT_REFRESH_WHITE_LIST_BROADCAST ="float_refresh_white_list_broadcast";
+
+    public static final String HAS_ADDED_LAUNCHER_AS_WHITE_LIST="has_added_launcher_as_white_list";
+
+
+    public static final String UNIVERSAL_COPY_BROADCAST="universal_copy_broadcast";
+    public static final String UNIVERSAL_COPY_BROADCAST_DELAY="universal_copy_broadcast_delay";
+    public static final String SCREEN_CAPTURE_OVER_BROADCAST="screen_capture_over_broadcast";
+
+
+    public static final String TOTAL_SWITCH_BROADCAST = "total_switch_broadcast";
+    public static final String MONITOR_CLICK_BROADCAST = "monitor_click_broadcast";
+    public static final String MONITOR_CLIPBOARD_BROADCAST = "monitor_clipboard_broadcast";
+
+    public static final String NOTIFY_UNIVERSAL_COPY_BROADCAST="notify_universal_copy_broadcast";
+    public static final String NOTIFY_SCREEN_CAPTURE_OVER_BROADCAST="notify_screen_capture_over_broadcast";
+
+    public static final String OCR_TIME = "ocr_time";
+    public static final int OCR_TIME_TO_ALERT = 5;
+    public static final String SHOULD_SHOW_DIY_OCR = "should_show_diy_ocr";
+
+    public static final String DIY_OCR_KEY="diy_ocr_key";
+
+
+    public static final String EFFECT_AFTER_REBOOT_BROADCAST ="effect_after_reboot_broadcast";
+
+    public static final String LONG_PRESS_KEY_INDEX ="long_press_key_index";
+
+    public static final String SHARE_APPS_DIS ="share_app_dis";
+    public static final String HAD_ENTER_INTRO="had_enter_intro";
+    public static final String SHARE_APP_INDEX = "share_app_index";
+
+    public static final String HAD_SHOW_LONG_PRESS_TOAST="had_show_long_press_toast";
+
+    //copyActivity
+    public static final String IS_FULL_SCREEN_COPY="is_full_screen_copy";
+
+    //feedback
+//    public static final String ALI_APP_KEY= BigBangApp.getInstance().getString(R.string.ali_feedback_key);
+    //xp全局复制
+    public static final String UNIVERSAL_COPY_BROADCAST_XP="universal_copy_broadcast_xp";
+
+}

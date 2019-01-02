eeb10a22b02ded34c0f3d42a538960497bcc969e
mmjang <752515918@qq.com>
2019-01-02 19:28:54 +0800

    note list activity

---
+++ app/src/main/java/com/mmjang/duckmemo/ui/browser/NoteBrowserActivity.java
@@ -0,0 +1,15 @@
+package com.mmjang.duckmemo.ui.browser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mmjang.duckmemo.R;
+
+public class NoteBrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_browser);
+    }
+}

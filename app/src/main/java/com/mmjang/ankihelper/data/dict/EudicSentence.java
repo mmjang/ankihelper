80ac6f2b4cbff5497ae149adf0478bf3ddf4061e
mmjang <752515918@qq.com>
2019-01-18 11:36:13 +0800

    show up to 40 sentences in eudic

--- app/src/main/java/com/mmjang/ankihelper/data/dict/EudicSentence.java
+++ app/src/main/java/com/mmjang/ankihelper/data/dict/EudicSentence.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import okhttp3.FormBody;
 import okhttp3.Request;
 
 /**
@@ -77,43 +78,27 @@ public class EudicSentence implements IDictionary {
             List<Definition> definitionList = new ArrayList<>();
 
             for(Element audioEle : doc.select("div.lj_item")){
-                HashMap<String, String> eleMap = new HashMap<>();
+                processOneSentence(key, audioEle, definitionList);
+            }
 
-                String fileName = key.trim() + "_" + Utils.getRandomHexString(8) + ".mp3";
-                String audioId = audioEle.attr("source");
-                audioId = URLDecoder.decode(audioId, "UTF-8");
-                audioId = audioId.replace("/","_");
-                String audioUrl = String.format("https://fs-gateway.esdict.cn/store_main/sentencemp3/%s.mp3", audioId);
-//                Elements a = audioEle.select("div.content > p.line a");
-//                String audioUrl = "";
-//                if(a.size() > 0){
-//                    audioUrl = "http://api.frdic.com/api/v2/speech/speakweb?" + a.get(0).attr("data-rel");
-//                }else{
-//                    continue;
-//                }
-                //audioUrl = URLDecoder.decode(audioUrl, "UTF-8");
-                String audioTag = String.format("[sound:%s]", Constant.AUDIO_SUB_DIRECTORY + File.separator + fileName);
-                String channelTitle = getSingleQueryResult(audioEle, "div.channel > span.channel_title", false);
-                String en = getSingleQueryResult(audioEle, "div.content > p.line", true);
-                en = en.replaceAll("<a href=.*?</a>$", "");
-                en = en.replaceAll("<span class=\"key\">(.*?)</span>", "<font color=blue>$1</font>");
-                String cn = getSingleQueryResult(audioEle, "div.content > p.exp", true);
-                cn = cn.replaceAll("<span class=\"key\">(.*?)</span>", "<font color=blue>$1</font>");
-                String html = String.format(tplt_card,
-                        en,
-                        audioTag,
-                        cn,
-                        "<font color=grey>" + channelTitle + "</font>"
-                        );
-
-                String html_ui = String.format(tplt_ui,
-                        en,
-                        cn,
-                        "<font color=grey>" + channelTitle + "</font>"
-                );
-                eleMap.put(EXP_ELE[0], key);
-                eleMap.put(EXP_ELE[1], html);
-                definitionList.add(new Definition(eleMap, html_ui, "", "", audioUrl, fileName));
+            Element load_more = doc.getElementById("liju_ting_loadmore");
+            if(load_more != null){
+                String pageStatus = doc.getElementById("page-status").attr("value");
+                FormBody formBody = new FormBody.Builder()
+                        .add("status", pageStatus)
+                        .add("start", "20")
+                        .add("type", "ting")
+                        .build();
+                Request request2 = new Request.Builder()
+                        .post(formBody)
+                        .url(wordUrl + key)
+                        .addHeader("User-Agent", Constant.UA)
+                        .build();
+                String rawhtml2 = MyApplication.getOkHttpClient().newCall(request).execute().body().string();
+                Document doc2 = Jsoup.parse("<html><body>" + rawhtml + "</body></html>");
+                for(Element audioEle : doc2.select("div.lj_item")){
+                    processOneSentence(key, audioEle, definitionList);
+                }
             }
 
             return definitionList;
@@ -124,6 +109,46 @@ public class EudicSentence implements IDictionary {
             return new ArrayList<Definition>();
         }
 
+    }
+
+    public void processOneSentence(String key, Element audioEle, List<Definition> definitionList) throws IOException{
+        HashMap<String, String> eleMap = new HashMap<>();
+
+        String fileName = key.trim() + "_" + Utils.getRandomHexString(8) + ".mp3";
+        String audioId = audioEle.attr("source");
+        audioId = URLDecoder.decode(audioId, "UTF-8");
+        audioId = audioId.replace("/","_");
+        String audioUrl = String.format("https://fs-gateway.esdict.cn/store_main/sentencemp3/%s.mp3", audioId);
+//                Elements a = audioEle.select("div.content > p.line a");
+//                String audioUrl = "";
+//                if(a.size() > 0){
+//                    audioUrl = "http://api.frdic.com/api/v2/speech/speakweb?" + a.get(0).attr("data-rel");
+//                }else{
+//                    continue;
+//                }
+        //audioUrl = URLDecoder.decode(audioUrl, "UTF-8");
+        String audioTag = String.format("[sound:%s]", Constant.AUDIO_SUB_DIRECTORY + File.separator + fileName);
+        String channelTitle = getSingleQueryResult(audioEle, "div.channel > span.channel_title", false);
+        String en = getSingleQueryResult(audioEle, "div.content > p.line", true);
+        en = en.replaceAll("<a href=.*?</a>$", "");
+        en = en.replaceAll("<span class=\"key\">(.*?)</span>", "<font color=blue>$1</font>");
+        String cn = getSingleQueryResult(audioEle, "div.content > p.exp", true);
+        cn = cn.replaceAll("<span class=\"key\">(.*?)</span>", "<font color=blue>$1</font>");
+        String html = String.format(tplt_card,
+                en,
+                audioTag,
+                cn,
+                "<font color=grey>" + channelTitle + "</font>"
+        );
+
+        String html_ui = String.format(tplt_ui,
+                en,
+                cn,
+                "<font color=grey>" + channelTitle + "</font>"
+        );
+        eleMap.put(EXP_ELE[0], key);
+        eleMap.put(EXP_ELE[1], html);
+        definitionList.add(new Definition(eleMap, html_ui, "", "", audioUrl, fileName));
     }
 
     public ListAdapter getAutoCompleteAdapter(Context context, int layout) {
